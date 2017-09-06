caad19c30e7184090c524a1d27a98c579416ca70
Andrii Kopp <kopp93@gmail.com>
2017-09-06 15:52:47 +0300

    Add XPDL to RDF console tool.

---
+++ XpdlToRdfTool/XpdlToRdf.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPMNAnalysisToolCore;
+
+namespace XpdlToRdfTool
+{
+    public class XpdlToRdf
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new Exception("Invalid XPDL document name!");
+            }
+
+            string xpdlDocument = args[0];
+
+            WorkflowProcesses workflowProcesses = new WorkflowProcesses()
+            {
+                Document = xpdlDocument
+            };
+
+            workflowProcesses.ReadDocument();
+
+            foreach (WorkflowProcess workflowProcess in workflowProcesses.Processes)
+            {
+                if (workflowProcess.Activities != null)
+                {
+                    workflowProcess.SaveAsRDFTriplesSet();
+                }
+            }
+        }
+    }
+}
